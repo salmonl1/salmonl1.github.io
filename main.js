442fb87b23834dbca2d3221983b56047414d3e03
Levi Salmon <salmonl1@msu.edu>
2018-10-09 21:49:56 -0400

    Final

--- main.js
+++ main.js
@@ -1,24 +1,34 @@
-/*var myVar = setInterval(hideDivs, 5000);
+var hideTime = setInterval(hideDivs, 5000);
 
 function hideDivs() {
     var random = Math.floor(Math.random() * $('.divgrid').length);
     var i = $('.divgrid').length;
     $('.divgrid').eq(random).css("opacity", "0").addClass("flickerout");
 }
-*/
-$('.divgrid').on('mouseover', function () {     
-    playWind();                                  //start wind sound
-})
-.on('mouseout', function () {                    
-    stopWind();                                  //stops the wind sound
-});
 
-function playWind () {                           //start wind audio
-    $('#beep-one')[0].volume = 0.7;
-    $('#beep-one')[0].load();
-    $('#beep-one')[0].play();
-  }
-  function stopWind () {                           //stop the wind audio
-    $('#beep-one')[0].pause();
-    $('#beep-one')[0].currentTime = 0;           //resets wind to zero/beginning
-  }
+
+function PlaySound(soundobj) {
+    var thissound=document.getElementById(soundobj);
+    thissound.play();
+}
+
+function StopSound(soundobj) {
+    var thissound=document.getElementById(soundobj);
+    thissound.pause();
+    thissound.currentTime = 0;
+}
+
+var alertTime = setInterval(fixAlert, 15000);
+
+function fixAlert() {
+    var codeFix = prompt("Please complete the missing segment\n.divgrid {\n     display:       ;\n}");
+    if (codeFix != "flex") {
+        alert("Sorry, I can't do that");
+        $('body').css("filter","grayscale(100)");
+    }
+    else {
+        alert("Acceptable.");
+        $('.divgrid').css("opacity", "1").removeClass("flickerout");
+        $('body').css("filter","grayscale(0)");
+    }
+}
